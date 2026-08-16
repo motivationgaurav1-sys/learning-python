d972c3bd99350a02cdb71186a7596b662fecaf18
Gaurav Pundir <motivation.gaurav1@gmail.com>
2026-08-16 07:18:01 +0000

    learn to use import files in python

---
+++ basics.py/print04.py
@@ -0,0 +1,6 @@
+"""learning how to imprt another file in this file,
+let suppose i have to print the second file of basics named 02_print_values.py 
+so i will not write it all again what will i do i will just import it.
+"""
+import print02 # here it will print the whole file as it is.
+print(print02.a) # here it will only print the value of x from the print02 file.
