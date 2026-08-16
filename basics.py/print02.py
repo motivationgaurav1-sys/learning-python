819317a42ec4b8d7395ba1662c81e5f81e49ca1d
Gaurav Pundir <motivation.gaurav1@gmail.com>
2026-08-16 06:26:17 +0000

    learn print multiple values in python

---
+++ basics.py/print02.py
@@ -0,0 +1,8 @@
+#print function - how to print multiple values.
+a=5
+b=6
+c=7
+print("a,b,c") #here we put a,b,c in inverted commas that why they got printed not there values.
+print(a,b,c) #here the value will print not the names.
+print(c,b,a)# reult will be 7 6 5
+print(c,a,c)# result will be 7 5 7
