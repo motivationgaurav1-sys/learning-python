e35efdbff1ecc6d0fdd5f54e05d99e2e0db23562
Gaurav Pundir <motivation.gaurav1@gmail.com>
2026-08-16 09:16:31 +0000

    learn to use arithmetic operators in python

---
+++ basics.py/operators06.py
@@ -0,0 +1,11 @@
+#learn how to use operators specifically arithmatic operators.
+print(2**8)
+a=2
+b=8
+print(a**b)
+print(4**3)
+print(b/a) #in python it will always give exact answer 
+print(3/4)
+print(10/2) #when it is fully divided it will no answer like 5 it will always answer in float form like 5.0 instead of 5.
+print(5/8)
+print(32/11)
