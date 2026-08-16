cd4dc2891504be2a5dd4fa15303989a68e55b24e
Gaurav Pundir <motivation.gaurav1@gmail.com>
2026-08-16 14:16:48 +0000

    learned about operations in python

---
+++ basics.py/operator08.py
@@ -0,0 +1,21 @@
+#lwan the differnce between float and int in power caluculation , multiply, divide,floor division, modulas/reminder 
+print(2**3)       # power
+print(4**8)       # power
+print(2.8**9)     #power
+print(2.8**9.7)    #power
+
+print(3*8)        # multiply
+print(3.5*8)      # multiply
+print(3.5*8.5)    # multiply
+
+print(10/2)       # divide
+print(10.5/2)     # divide
+print(10.5/2.7)   # divide
+
+print(10//2)      # floor divide
+print(10.5//2)    # floor divide
+print(10.5//2.7)  # floor divide
+
+print(5%2)        # modulo/remainder
+print(5.5%2)      # modulo/remainder
+print(5.5%2.5)    # modulo/remainder
