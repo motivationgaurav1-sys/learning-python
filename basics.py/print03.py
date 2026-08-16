34f30889c8e6ddeeb1ac5961623e54bb20b3d69b
Gaurav Pundir <motivation.gaurav1@gmail.com>
2026-08-16 06:36:26 +0000

    learn to use end and sep in python

---
+++ basics.py/print03.py
@@ -0,0 +1,9 @@
+#print function - how to use end and sep.
+a=5
+b=3
+c=8
+print(a,b,c,sep="-")
+print("a is",a,"b is",b)
+print(a,b,c,end="^")# here end means don't print in next line instead of put the symbol given in inverted commas and than just write the input written in the next line.
+print("hello")
+print("byy")
