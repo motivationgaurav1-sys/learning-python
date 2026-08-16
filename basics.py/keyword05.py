80e18007a1e6dc6032782113b24fba62d480d1a3
Gaurav Pundir <motivation.gaurav1@gmail.com>
2026-08-16 09:04:00 +0000

    learn to print all keywords in python

---
+++ basics.py/keyword05.py
@@ -0,0 +1,5 @@
+#learn to print all the keywords in python.
+#also to know much total keywords are there.
+import keyword
+print(keyword.kwlist)
+print("there are total",len(keyword.kwlist),"keywords in python")
