68231b972b28945571ca5c8288f100fbe984ee6a
Gaurav Pundir <motivation.gaurav1@gmail.com>
2026-08-19 12:49:31 +0000

    learn to use ifelifelse in python

---
+++ decisioncontrol.py/ifelifelse04.py
@@ -0,0 +1,16 @@
+"""write a program  to print grade
+obtained in a test, take marks obtained 
+from user and display the grade."""
+x=int(input("enter your marks "))
+if 90<x<=100:
+    print("A grade")
+elif 80<x<=90:
+    print("B grade")
+elif 70<x<=80:
+    print("C grade")    
+elif 60<x<=70:
+    print("D grade")
+elif 50<=x<=60:
+    print("E grade")
+else:
+    print("below 50")       
