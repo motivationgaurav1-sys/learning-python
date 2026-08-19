4349ba13367d025eb1e21d8278178fd8baa6232f
Gaurav Pundir <motivation.gaurav1@gmail.com>
2026-08-19 12:22:29 +0000

    learn to amke program with if in python

---
+++ decisioncontrol.py/if02.py
@@ -0,0 +1,8 @@
+#write a program to check wheather a given number is positive or non positive
+print("enter a number  ")
+x=int(input()) 
+# or we can write x=int(input("emnter a number  "))
+if x>0:
+    print("Positive number")
+if x<=0:
+    print("non posiive")    
