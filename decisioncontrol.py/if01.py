987a86d2b53287aa2f4fac032d8c22f003d4779f
Gaurav Pundir <motivation.gaurav1@gmail.com>
2026-08-19 12:07:01 +0000

    lean about if decision control in python

---
+++ decisioncontrol.py/if01.py
@@ -0,0 +1,12 @@
+x=-2
+if x>0:
+    print("hello")
+    print("mysirg")
+    #boh of the statement hello and my sir g will not run because the consition is not true.
+print("bye")    # this will always run becuse there is no condition applyied on this statement it is not inside if block.
+x=2
+if x>0:
+    print("Hello")
+    print("my sir g")
+    #here both of them will run because the condition are true.
+
