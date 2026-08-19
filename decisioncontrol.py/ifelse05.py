854ee95435a52acadb3f74eb55c4343e35e43500
Gaurav Pundir <motivation.gaurav1@gmail.com>
2026-08-19 18:36:40 +0530

    ifelse05.py

--- decisioncontrol.py/ifelse05.py
+++ decisioncontrol.py/ifelse05.py
@@ -1,3 +1,5 @@
 # write a program to check weather a program is even or odd.
-x=int(input("Enter a number  "))
+"""x=int(input("Enter a number  "))
 print("Even") if x%2==0 else print("Odd")
+this is short but there is also an another way even more shorter"""
+print("even" if int(input"enter a number"))%2==0 else "odd")
