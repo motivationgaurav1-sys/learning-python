82c836f04fb14d1bcea5ddcfa8d80488ebcbaffd
Gaurav Pundir <motivation.gaurav1@gmail.com>
2026-09-07 18:00:34 +0000

    some basics of pyhton

---
+++ basics.py/basic12.py
@@ -0,0 +1,31 @@
+#single line comment ..
+"""" doc dtring commment in pyhton
+
+
+"""
+a=10
+b=20
+c=30
+x=y=z=50
+print(a+b)
+print(a-b)
+print(a*b)
+print(a%b)
+print(a//b)
+print(a**b)
+print(a/b)
+a=5.6
+b=2.7
+print(a+b)
+print(a-b)
+print(a*b)
+print(a%b)
+print(a//b)
+print(a**b)
+print(a/b)
+x=int(input())
+y=int(input())
+print(x<y)
+print(x>y)
+
+
