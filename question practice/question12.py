4d18f9d93fcc2a59562817c396d491e006f39eae
Gaurav Pundir <motivation.gaurav1@gmail.com>
2026-09-24 17:37:03 +0000

    basic question- digital storage calculator

---
+++ question practice/question12.py
@@ -0,0 +1,10 @@
+file=4.7
+file_in_mb=4.7*1024
+print(file_in_mb)
+mb=7/100*file_in_mb
+total_space=file_in_mb+mb
+print(total_space)
+drive_space=5120
+remaining_space=drive_space-total_space
+print(remaining_space)
+print("the drive does not have enough space; it is horted by 29.696 MB.")
