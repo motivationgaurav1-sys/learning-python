97f2aea595dbd91dc0476832b0dce27eab0634dd
Gaurav Pundir <motivation.gaurav1@gmail.com>
2026-09-24 17:23:11 +0000

    basic questin- exam score normalizer

---
+++ question practice/question11.py
@@ -0,0 +1,15 @@
+#exam score normalizer
+mathematics=87
+physics=74
+pyhton=91
+communication=68
+total_marks=87+74+91+68
+print(total_marks)
+total_maximum_marks=100+80+100+75
+percentage=total_marks/total_maximum_marks*100
+print(percentage)
+
+final_max_marks=total_maximum_marks+50
+required_total_marks=final_max_marks*85/100
+marks_needed=required_total_marks-total_marks
+print(marks_needed)
