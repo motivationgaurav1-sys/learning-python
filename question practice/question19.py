a520aac4ecb5514ab10999608f13b043a081e11a
Gaurav Pundir <motivation.gaurav1@gmail.com>
2026-09-25 19:58:29 +0000

    sum of even natural numbers

---
+++ question practice/question19.py
@@ -0,0 +1,8 @@
+#find sum of even natural numbers
+n=int(input())
+i=1
+s=0
+while i<=n:
+    s=s+i
+    i+=1
+print(s)    
---
+++ question practice/question20.py
@@ -0,0 +1,4 @@
+#multiplication table
+x=int(input())
+for i in range(1,11):
+    print("{}*{}={}".format(x,i,x*i))
